d025313852a0089410513771a4e99faf38b1ef3f
EdwinAbdullah <e.abdullah@student.rug.nl>
2018-01-26 16:03:10 +0100

    adding a 1

---
+++ commiter.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import os
+
+EmptyFile = open("counter", 'a')
+
+num =range(0,250)
+
+for i in num:
+	os.system('git pull')
+	EmptyFile.write("1")
+	os.system('git add --all :/')
+	os.system('git commit -m "adding a 1"')
+	os.system('git push')
+
+EmptyFile.close()
+
+print "You are now commited"
